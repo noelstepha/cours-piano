01a7e75a856ef6705b8bf298793551afae082f13
stephanienoel <snoel@carlipa.com>
2022-08-23 13:37:03 +0200

    fix margin

--- src/components/PagePreview.vue
+++ src/components/PagePreview.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-      class="m-auto max-w-screen-xl flex"
+      class="mx-5 md:mx-10 2xl:mx-auto max-w-screen-xl flex items-stretch"
       :class="{ 'flex-col-reverse md:flex-row': reverse, 'flex-wrap' : !reverse }"
   >
     <div
-        class="w-full md:w-1/2 h-80 bg-center bg-cover"
+        class="w-full md:w-1/2 h-80 md:h-auto bg-center bg-cover"
         v-if="!reverse"
         :class="{ [bgClass]: true }"
     ></div>
-    <div class="w-full md:w-1/2 py-5 pl-10 flex flex-col"
-    :class="{ 'pt-10': !first, 'pb-10': !last, 'xl:pl-0': reverse, 'pr-10': reverse} ">
+    <div class="w-full md:w-1/2 py-5 pr-5 flex flex-col"
+    :class="{ 'pt-10': !first, 'pb-10': !last, 'md:pl-10': !reverse} ">
       <Title :text="title"></Title>
       <p class="flex-grow">
         <slot />
@@ -19,7 +19,7 @@
       </Button>
     </div>
     <div
-        class="w-full md:w-1/2 h-80 bg-center bg-cover"
+        class="w-full md:w-1/2 h-80 md:h-auto bg-center bg-cover"
         v-if="reverse"
         :class="{ [bgClass]: true }"
     ></div>
